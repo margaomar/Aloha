03720095e6cc8a10401c9c815c563fb74e960887
Marga Omar <margaomar@gmail.com>
2018-10-15 20:05:26 +0100

    Final commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Project: Ahola Apparel Co.
+## About this project
+You can see this project on line, going to  [Alohaapparel.com](http://Alohaapparel.com)
+
+----
+## Goals of the site
+This site has been created for the commercial diffusion of the brand Aloha Apparel Co.
+
+
+> Hand-Crafter Styles
+Aloha Apparel Co. sells only the finest ready-to-travel clothing. Browse our current selectin of products or sign-up for updates below.
+
+----
+## Technology used
+HTML5 | CSS3 | JQuery | Responsive design | Mobile First | Flickity Carousel | Validate CSS | Terminal Lines | Github | Git | FontFace |
+
+
+
+----
+## Personal Learnings
+
+1. JQuery Events, DOM, Methods
+2. Flickity Carousel
+3. Validate CSS
+4. Comand Line using iTerm2
+5. Git and Github
+6. FontFace
+7. Flexbox
+8. Read until the end of the requirements 
+
+----
+## Link to Github
+* **[Ahola Apparel on Github](https://github.com/margaomar/Aloha)**
+
+----
+## Dead Line for the project
+* 16-Oct-2018
+
+----
+## Project developed by 
+ **[Marga Omar]
+(https://margaomar.com)**
---
+++ js/jquery-custom.js
@@ -0,0 +1,91 @@
+  
+    $('.carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true
+    });
+
+    // newsletter message
+    $(".newsletter").on('submit','form',function(event){
+        event.preventDefault();
+        const $emailInput = $('#your-email');
+        if ($emailInput.val().length!==0){
+            alert("Thanks for suscribing");
+        } else {
+            alert("Please, enter a correct email address");
+        }
+    });
+
+    //smooth-scrolling
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            };
+        });
+        }
+    }
+    });
+
+    // fixed nav
+    var wrap = $("#header");
+
+    wrap.on("scroll", function(e) {
+        
+    if (this.scrollTop > 147) {
+        wrap.addClass("fix-header");
+    } else {
+        wrap.removeClass("fix-header");
+    }
+    
+    });
+
+    //hide menu scroll down
+    
+        'use strict';
+        
+         var c, currentScrollTop = 0,
+             navbar = $('header');
+      
+         $(window).scroll(function () {
+            var a = $(window).scrollTop();
+            var b = navbar.height();
+           
+            currentScrollTop = a;
+           
+            if (c < currentScrollTop && a > b + b) {
+              navbar.addClass("scrollUp");
+            } else if (c > currentScrollTop && !(a <= b)) {
+              navbar.removeClass("scrollUp");
+            }
+            c = currentScrollTop;
+        });
+
